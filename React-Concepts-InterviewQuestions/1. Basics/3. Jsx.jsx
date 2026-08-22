7f783c44c24f70e99661515a56aecb17a6bef6f0
Mohd Arif <ariflucky1@gmail.com>
2026-08-22 23:59:45 +0530

    JSX

---
+++ React-Concepts-InterviewQuestions/1. Basics/3. Jsx.jsx
@@ -0,0 +1,10 @@
+
+    What is JSX ??
+
+    JSX is a syntax extension for JavaScript.
+    It allows us to write HTML-like code inside JavaScript.
+
+
+    ex:
+        const element = <h1 className="title">Hello React!</h1>
+        function App
