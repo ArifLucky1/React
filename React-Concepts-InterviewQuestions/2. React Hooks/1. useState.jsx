8cd415887fbadca59bcf482ad968ae1f04f0506c
Mohd Arif <ariflucky1@gmail.com>
2026-08-25 14:26:02 +0530

    Hooks-useSate

---
+++ React-Concepts-InterviewQuestions/2. React Hooks/1. useState.jsx
@@ -0,0 +1,15 @@
+/*
+    useState
+        Add state to your component.
+        Triggers re-render on update.
+*/
+
+import { useState } from "react";
+
+
+    const [count, setCount] = useState(0);
+
+
+
+
+//   Use it when:  You need to store and update state in your component.
