a0a2129690b122ec70fa8fe30606ba3c1a57bdbb
Mohd Arif <ariflucky1@gmail.com>
2026-08-25 14:33:53 +0530

    Hooks-useEffect

---
+++ React-Concepts-InterviewQuestions/2. React Hooks/2. useEffect.jsx
@@ -0,0 +1,20 @@
+/* 
+    useEffect
+        Run side effects in function components.
+*/
+
+import { useEffect } from "react";
+
+
+
+
+        useEffect(() => {
+            // side effect
+        }, [deps]);
+
+
+
+
+
+
+//     Use it when:   You need to fetch data, subscribe and sync with external systems. 
