53af6109455f1c0c4f454586eee0f1aa5ee48cbb
Mohd Arif <ariflucky1@gmail.com>
2026-08-26 23:11:40 +0530

    props

---
+++ React-Concepts-InterviewQuestions/1. Basics/6. Props.jsx
@@ -0,0 +1,13 @@
+
+//   What are props ??
+//      Props (short for properties) are read-only inputs passed from parennt component to child component.
+
+
+
+        function Child(props){
+            return <h1>Hello, {props.name}</h1>;
+        }
+
+        function App(){
+            return <Child name="Arif" />
+        }
