76732d8deaa3a8b5c6bcd8fb2426b543ec9d14ad
Mohd Arif <ariflucky1@gmail.com>
2026-08-28 22:58:36 +0530

    Hooks-useContext.

---
+++ React-Concepts-InterviewQuestions/3. Hooks/6. useContext.jsx
@@ -0,0 +1,9 @@
+
+//      What is useContext ??
+//          useContext lets you access context value in functional components.
+
+
+        import {useContext} from 'react'; 
+        const value = useContext(MyContext);
+
+//        ***   Avoids prop drilling    ***
