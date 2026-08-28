fd6af6b4050c522cc31f0ab1ddecfc3dcdaafec3
Mohd Arif <ariflucky1@gmail.com>
2026-08-28 23:12:07 +0530

    Hooks-Rules.

--- React-Concepts-InterviewQuestions/3. Hooks/7. Custom-Hooks.jsx
+++ React-Concepts-InterviewQuestions/3. Hooks/7. Custom-Hooks.jsx
@@ -18,3 +18,16 @@
 
 
 //      ***  Helps in reusing logic across components.   ***
+
+
+
+//      What are the Rules of Hooks ??
+//          Rules ensure Hooks work correctly.
+
+//      Rules:
+//         - Only call Hooks at the top level.
+//         - Only call Hooks from React function (components or custom Hooks).
+
+
+
+//      ***  Helps React rely on the Hook call order.   ***
