1eb1531057a6778dc49e0fafe002560e02f4f42b
Mohd Arif <ariflucky1@gmail.com>
2026-08-30 23:46:25 +0530

    Code Splitting

---
+++ React-Concepts-InterviewQuestions/4. Performance/4. Code-splitting.jsx
@@ -0,0 +1,14 @@
+
+//      What is Code Splitting ??
+//          Code splitting is a technique to split your code into smaller bundles which can be loaded on demand.
+
+
+//  Ex: 
+//      Using React.Lazy()
+//      Dynamic import()
+//      Route based splitting
+
+
+
+
+//      ***  Leads to faster load time and better user experience.   ***
