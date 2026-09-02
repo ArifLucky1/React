bececfe3c287a56ff8379653f83a7ec42658086b
Mohd Arif <ariflucky1@gmail.com>
2026-09-02 19:56:51 +0530

    Lifting State Up

---
+++ React-Concepts-InterviewQuestions/4. Performance/8. Lifting-State-Up.jsx
@@ -0,0 +1,14 @@
+
+//  What is Lifting State Up ??
+//      Lifting State Up means moving state to the closest common ancestor so that multiple components can share it.
+
+
+//                   Parent
+//                   (State)
+//
+//  Child A     ----------------->          Child B
+//                  share state
+
+
+
+//      ***  Helps in sharing and syncing state between components.    ***
